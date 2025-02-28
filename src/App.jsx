469b9fc1cd8b42535f06660458f615d00a955e69
Larry Quantz <lquantz@gmail.com>
2025-02-28 01:43:34 -0800

    changed background

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect, useState, Suspense } from "react";
+import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import {
   AccumulativeShadows,
@@ -12,6 +13,7 @@ import {
   Html,
   Billboard,
   Text,
+  useEnvironment,
 } from "@react-three/drei";
 //import { EffectComposer, Bloom, HueSaturation, BrightnessContrast, TiltShift2, WaterEffect, ToneMapping } from '@react-three/postprocessing'
 import Egg from "./Egg";
@@ -34,12 +36,18 @@ export default function App() {
       shadows
       camera={{ position: [0, -11.5, 15], fov: 35 }}
     >
-      <color attach="background" args={["#353535"]} />
+      {/* <color attach="background" args={["#353535"]} /> */}
       {/* <fog attach="fog" args={["#353535", 5, 20]} /> */}
       <ambientLight intensity={2.5} />
       <Suspense fallback={<Loader3 />}>
         <Egg position={[1, -0.225, 1]} scale={1.5} rotation={[-0.1, 0, 0]} />
-        <Environment preset="city" />
+        <Environment
+          preset="dawn"
+          background
+          backgroundBlurriness={0.5}
+          backgroundIntensity={0.3}
+        ></Environment>
+
         <OrbitControls
           autoRotate={false}
           autoRotateSpeed={0.9}
@@ -49,20 +57,6 @@ export default function App() {
           minAzimuthAngle={-Math.PI / 8}
           maxAzimuthAngle={Math.PI / 8}
         />
-        {/* <Html
-          position={[-4, 2, 0]} // Initial 3D position (adjusted by transform)
-          transform={false} // Prevents it from being affected by 3D transforms
-          style={{
-            position: "absolute",
-            top: "10px", // Distance from top of canvas
-            left: "10px", // Distance from left of canvas
-            color: "white",
-            fontSize: "20px",
-            pointerEvents: "none", // Prevents interaction with the text
-          }}
-        >
-          Upper Left Text
-        </Html> */}
 
         {/* <Sparkles color="white" count={1200} scale={10} size={1.5} /> */}
       </Suspense>
